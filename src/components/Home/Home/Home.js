6e8e66da49d0e940743a8d4cdfed14d5ded4f216
irtiza1999 <mdirtiza.hossain1999@gmail.com>
2020-12-12 22:16:41 +0600

    home almost done

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import Blogs from '../Blogs/Blogs'
+import Contact from '../Contact/Contact'
+import Doctors from '../Doctors/Doctors'
 import FeaturedServices from '../FeaturedServices/FeaturedServices'
+import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import MakeAppoinment from '../MakeAppoinment/MakeAppoinment'
 import Services from '../Services/Services'
+import Testimonials from '../Testimonials/Testimonials'
 
 const Home = () => {
   return (
@@ -11,6 +16,11 @@ const Home = () => {
       <Services></Services>
       <FeaturedServices></FeaturedServices>
       <MakeAppoinment></MakeAppoinment>
+      <Testimonials></Testimonials>
+      <Blogs></Blogs>
+      <Doctors></Doctors>
+      <Contact></Contact>
+      <Footer></Footer>
     </div>
   )
 }
